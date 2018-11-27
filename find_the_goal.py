64780a90464574414af3c1e7dd509925da0742fd
Matthew Reynard <matthewreynard24@gmail.com>
2018-11-27 17:34:25 +0200

    Added pytorch model start

---
+++ find_the_goal.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+# $MALMO_MINECRAFT_ROOT/launchClient.sh -port 10000
+
+from __future__ import print_function
+
+import os, sys
+sys.path.insert(0, os.getcwd()) 
+
+from builtins import range
+from malmo import MalmoPython
+import time
+import json
+import random
+import numpy as np
+import torch
+# import tensorflow as tf
+from model import DeepQNetwork, Agent
+
+if sys.version_info[0] == 2:
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+else:
+    import functools
+    print = functools.partial(print, flush=True)
+
+# import malmo.minecraftbootstrap
+# malmo.minecraftbootstrap.set_malmo_xsd_path()
+
+if "MALMO_XSD_PATH" not in os.environ:
+	os.environ["MALMO_XSD_PATH"] = "/home/matthew/Malmo/Schemas"
+
+if "MALMO_MINECRAFT_ROOT" not in os.environ:
+	os.environ["MALMO_MINECRAFT_ROOT"] = "/home/matthew/Malmo/Minecraft"
+
+if "JAVA_HOME" not in os.environ:
+	os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+
+
+# Get the agent state
+def get_state(grid):
+    '''Get the state in the same way snake was'''
+
+    state = torch.zeros([3, 9, 9], dtype=torch.float32)
+    
+    state[0,4,4] = 1
+    
+    for i in range(9*9):
+        if grid[i] == "gold_block":
+            state[1, 8-int(i/9), 8-int(i%9)] = 1
+        if grid[i] == "cobblestone" or grid[i] == "stone":
+            state[2, 8-int(i/9), 8-int(i%9)] = 1
+
+    return state
+    # needs to be put to cpu before you can convert it to numpy array
+
+    # DEBUGGING
+    # print(state.numpy())
+
+# Reset the Minecraft environment, return the world state
+def reset(agent, max_retries=3):
+    for retry in range(max_retries):
+        try:
+            agent.startMission(my_mission, my_mission_record)
+            break
+        except RuntimeError as e:
+            if retry == max_retries - 1:
+                print("Error starting mission:", e)
+                exit(1)
+            else:
+                time.sleep(2.5)
+
+    # Loop until mission starts:
+    print("Waiting for the mission to start ", end=' ')
+    world_state = agent_host.getWorldState()
+    while not world_state.has_mission_begun:
+        print(".", end="")
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+        for error in world_state.errors:
+            print("Error:", error.text)
+
+    print()
+    print("Mission running ", end=' ')
+    
+    return world_state
+
+
+action_space = ["move 1", "move -1", "strafe 1", "strafe -1"]
+# action_space = ["move 0", "move 1", "move -1", "strafe 0", "strafe 1", "strafe -1"]
+
+brain = Agent(gamma = 0.99, epsilon = 1.0, alpha = 0.001, maxMemorySize = 100, replace = None)
+
+# Create default Malmo objects:
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print('ERROR:',e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+
+# -- set up the mission -- #
+mission_file = './find_the_goal.xml'
+with open(mission_file, 'r') as f:
+    print("Loading mission from %s" % mission_file)
+    mission_xml = f.read()
+    my_mission = MalmoPython.MissionSpec(mission_xml, True)
+
+# Python code alterations to the environment
+# my_mission.drawBlock(0, 110, 0, "stone")
+
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+# Attempt to start a mission:
+max_retries = 3
+
+print(my_mission.getSummary())
+
+# INITIALISING REPLAY MEMORY
+while brain.memCntr < brain.memSize:
+    world_state = reset(agent_host)
+
+    getting_obs = True
+    while getting_obs:
+        world_state = agent_host.getWorldState()
+        # print(world_state)
+        if world_state.number_of_observations_since_last_state > 0:
+            getting_obs = False
+        
+    msg = world_state.observations[-1].text
+    observations = json.loads(msg)
+    grid = observations.get(u'floor9x9', 0)
+    state = get_state(grid)
+
+    while world_state.is_mission_running: 
+        print("-", end="")
+        time.sleep(0.1)
+
+        # action based on current state
+        # print(state)
+        action_taken = np.random.choice(len(action_space))
+        agent_host.sendCommand(action_space[action_taken])
+
+        world_state = agent_host.getWorldState()
+
+        for error in world_state.errors:
+            print("Error:", error.text)
+
+        # Have we received any observations
+        if world_state.number_of_observations_since_last_state > 0 and world_state.number_of_rewards_since_last_state > 0:
+
+            msg = world_state.observations[-1].text
+            observations = json.loads(msg)
+            grid = observations.get(u'floor9x9', 0)
+            
+            # Get new_state & reward
+            new_state = get_state(grid)
+            reward = world_state.rewards[-1].getValue()
+            
+            # Store the transitions
+            brain.storeTransition(state, action_taken, reward, new_state)
+
+            # Not neccesrily true, might be a few observations after
+            state = new_state
+
+print("Done initialising memory")
+
+# scores = []
+epsHistory = []
+num_episodes = 10
+batch_size = 16
+
+avg_score = 0
+avg_loss = 0
+
+# TRAINING
+for i in range(num_episodes):
+
+    epsHistory.append(brain.EPSILON)
+    score = 0
+
+    # reset env
+    world_state = reset(agent_host)
+    # print(world_state)
+
+    # for i in range(len(my_mission.getListOfCommandHandlers(0))):
+    #     print(my_mission.getListOfCommandHandlers(0)[i])
+
+    # for action in my_mission.getAllowedCommands(0,"DiscreteMovement"):
+    #     print(action)
+
+    getting_obs = True
+    while getting_obs:
+        world_state = agent_host.getWorldState()
+        # print(world_state)
+        if world_state.number_of_observations_since_last_state > 0:
+            getting_obs = False
+        
+    msg = world_state.observations[-1].text
+    observations = json.loads(msg)
+    grid = observations.get(u'floor9x9', 0)
+    state = get_state(grid)
+
+    # Loop until mission ends
+    while world_state.is_mission_running: 
+        print("-", end="")
+        time.sleep(0.1)
+
+        # action based on current state
+        # print(state)
+        action_taken = np.random.choice(len(action_space))
+        agent_host.sendCommand(action_space[action_taken])
+
+        world_state = agent_host.getWorldState()
+
+        for error in world_state.errors:
+            print("Error:", error.text)
+
+        # Have we received any observations
+        if world_state.number_of_observations_since_last_state > 0 and world_state.number_of_rewards_since_last_state > 0:
+
+            msg = world_state.observations[-1].text
+            observations = json.loads(msg)
+            grid = observations.get(u'floor9x9', 0)
+            
+            # Get new_state & reward
+            new_state = get_state(grid)
+            reward = world_state.rewards[-1].getValue()
+            score += reward
+            
+            # Store the transitions
+            brain.storeTransition(state, action_taken, reward, new_state)
+
+            # Not neccesrily true, might be a few observations after
+            state = new_state
+
+            loss = brain.learn(batch_size)
+
+    avg_score += info["score"]
+    avg_loss += loss.item()
+            	
+    print()	
+    print("Mission {} ended".format(i+1))
+    brain.save_model("./Models/Torch/my_model{}.pth".format(i))
+    # Mission has ended.
+
+print()
+print("Done")
+
+brain.save_model("./Models/Torch/my_model.pth")
--- model.py
+++ model.py
@@ -7,24 +7,24 @@ import numpy as np
 class DeepQNetwork(nn.Module):
 	def __init__(self, ALPHA):
 		super(DeepQNetwork, self).__init__()
-		self.conv1 = nn.Conv2d(1, 32, 8, stride=4, padding=1)
-		self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
-		self.conv3 = nn.Conv2d(64, 128, 3)
-		self.fc1 = nn.Linear(128*19*8, 512)
-		self.fc2 = nn.Linear(512, 6)
+		self.conv1 = nn.Conv2d(3, 16, 3, stride=1, padding=0)
+		self.conv2 = nn.Conv2d(16, 32, 3, stride=1)
+		# self.conv3 = nn.Conv2d(64, 128, 3)
+		self.fc1 = nn.Linear(3*3*32, 256)
+		self.fc2 = nn.Linear(256, 4)
 
-		self.optimiser = optim.RMSprop(self.parameters(), lr=ALPHA)
+		self.optimiser = optim.SGD(self.parameters(), lr=ALPHA)
 		self.loss = nn.MSELoss()
 		self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
 		self.to(self.device)
 
 	def forward(self, observation):
 		observation = T.Tensor(observation).to(self.device)
-		observation = observation.view(-1, 1, 185, 95) # size of image 185 x 95
+		observation = observation.view(-1, 3, 9, 9) # size of image 185 x 95
 		observation = F.relu(self.conv1(observation))
 		observation = F.relu(self.conv2(observation))
-		observation = F.relu(self.conv3(observation))
-		observation = observation.view(-1, 128*19*8) # flatten
+		# observation = F.relu(self.conv3(observation))
+		observation = observation.view(-1, 3*3*32) # flatten
 		observation = F.relu(self.fc1(self.observation))
 
 		actions = self.fc2(observation)
@@ -32,7 +32,7 @@ class DeepQNetwork(nn.Module):
 		return actions
 
 class Agent(object):
-	def __init__(self, gamma, epsilon, alpha, maxMemorySize, epsEnd = 0.05, replace=10000, actionSpace=[0,1,2,3,4,5]):
+	def __init__(self, gamma, epsilon, alpha, maxMemorySize, epsEnd = 0.05, replace=10000, actionSpace=[0,1,2,3]):
 		self.GAMMA = gamma
 		self.EPSILON = epsilon
 		self.EPS_END = epsEnd
@@ -45,7 +45,7 @@ class Agent(object):
 		self.Q_eval = DeepQNetwork(alpha)
 		self.Q_next = DeepQNetwork(alpha)
 
-	def storeTransisition (self, state, action, reward, state_):
+	def storeTransition(self, state, action, reward, state_):
 		if self.memCntr < self.memSize:
 			self.memory.append([state, action, reward, state_])
 		else:
@@ -56,7 +56,7 @@ class Agent(object):
 		rand = npp.random.random()
 		actions = self.Q_eval.forward(observation)
 		if rand < 1 - self.EPSILON:
-			action = T.argmax(actions[1]).item()
+			action = T.argmax(actions[0]).item()
 		else:
 			action = np.random.choice(self.actionSpace)
 		self.steps += 1
@@ -71,23 +71,29 @@ class Agent(object):
 		if self.replace_target_cnt is not None and self.learn_step_counter % self.replace_target_cnt == 0:
 			self.Q_next.load_state_dict(self.Q_eval.state_dict()) # not used
 
-		if self.memCntr + batch_size < self.memSize:
-			memStart = int(np.randon.choice(range(self.memCntr - batch_size - 1)))
+		if self.memCntr+batch_size < self.memSize:
+			memStart = int(np.random.choice(range(self.memCntr)))
+		else:
+			memStart = int(np.random.choice(range(self.memSize-batch_size-1)))
 
 		miniBatch = self.memory[memStart:memStart+batch_size]
-		memory = np.array(miniBatchi)
+		print(miniBatch)
+		memory = np.array(miniBatch)
 
 		Qpred = self.Q_eval.forward(list(memory[:, 0][:])).to(self.Q_eval.device)
 		Qnext = self.Q_next.forward(list(memory[:, 3][:])).to(self.Q_eval.device)
 
 		maxA = T.argmax(Qnext, dim=1).to(self.Q_eval.device)
 		reward = T.Tensor(list(memory[:,2])).to(self.Q_eval.device)
-		Qtarget = Qpred
-		Qtarget[:, maxA] = reward + self.GAMMA*T.max(Qnext[1])
+		# Qtarget = Qpred
+		Qtarget = Qpred.clone()
+		# Qtarget[:, maxA] = reward + self.GAMMA*T.max(Qnext[1])
+		for i in range(batch_size):
+			Qtarget[i][maxA[i]] = reward[i] + self.GAMMA*T.max(Qnext[i])
 
 		# linear decrease of epsilon
 		if self.steps > 500:
-			if self.EPSILON = 1e-4 > self.EPS_END:
+			if self.EPSILON - 1e-4 > self.EPS_END:
 				self.EPSILON -= 1e-4
 			else:
 				self.EPSILON = self.EPS_END
@@ -96,3 +102,14 @@ class Agent(object):
 		loss.backward() # back propagate
 		self.Q_eval.optimiser.step()
 		self.learn_step_counter += 1
+
+	def save_model(self, name):
+		try:
+			os.makedirs('./Models/Torch', exist_ok=True)
+		except Exception:
+			print('Could not create directory ./Models/Torch')
+
+		T.save(self.Q_eval.state_dict(), name)
+
+	def load_model(self, name):
+		self.Q_eval.load_state_dict(T.load(name))
